fb18be428c7a21bef079dfb4540a3a66c95702a2
Nils Helming <nilshelming@gmail.com>
2021-04-08 08:35:30 +0200

    Aufg_1: Vollständige Lösung

--- Workspace/Aufg_1/src/main.cpp
+++ Workspace/Aufg_1/src/main.cpp
@@ -7,6 +7,15 @@ std::unique_ptr<Konto> createOwnerAccount(const std::string& name, std::uint32_t
     return std::make_unique<Konto>(account, name);
 }
 
+std::shared_ptr<Konto> createOwnerAccount_shared(const std::string& name, std::uint32_t account){
+    return std::make_shared<Konto>(account, name);
+}
+
+// SRC(shared) -> Konto *Bestimmt Lebenszeit des Kontos
+// Konto(unique) -> Kunde *Bestimmt Lebenszeit des Kundes
+// Kunde(*) -> Konto *Bestimmt garnichts
+
+
 int main(){
     std::cout << "Start" << std::endl;
 
